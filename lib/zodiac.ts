954fecaa569ce52f229c8e47a320b00f3b247392
Antonio97-key <antoniocorporan40@gmail.com>
2026-03-07 16:41:00 -0400

    Update zodiac.ts

--- lib/zodiac.ts
+++ lib/zodiac.ts
@@ -1,20 +1,20 @@
 export function getSignFromDate(day: number, month: number): string {
-  if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) return 'Aries';
-  if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) return 'Tauro';
-  if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) return 'Géminis';
-  if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) return 'Cáncer';
-  if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) return 'Leo';
-  if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) return 'Virgo';
-  if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) return 'Libra';
-  if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) return 'Escorpio';
-  if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) return 'Sagitario';
-  if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) return 'Capricornio';
-  if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) return 'Acuario';
-  if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) return 'Piscis';
-  return '';
+    if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) return 'Aries';
+    if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) return 'Tauro';
+    if ((month === 5 && day >= 21) || (month === 6 && day <= 20)) return 'Géminis';
+    if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) return 'Cáncer';
+    if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) return 'Leo';
+    if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) return 'Virgo';
+    if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) return 'Libra';
+    if ((month === 10 && day >= 23) || (month === 11 && day <= 21)) return 'Escorpio';
+    if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) return 'Sagitario';
+    if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) return 'Capricornio';
+    if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) return 'Acuario';
+    if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) return 'Piscis';
+    return '';
 }
 
 export function getZodiacSign(day: number, month: number) {
-  const name = getSignFromDate(day, month);
-  return { name };
+    const name = getSignFromDate(day, month);
+    return { name };
 }
